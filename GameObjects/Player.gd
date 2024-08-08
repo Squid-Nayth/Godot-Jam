1e22b5cd4ea8cd9d927c5c57dc39d802874134b5
Nathan Michel <nathannkombe@gmail.com>
2024-08-08 03:32:54 +0100

    Handle ui_cancel action to toggle mouse visibility in _process()

--- GameObjects/Player.gd
+++ GameObjects/Player.gd
@@ -24,3 +24,8 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	animation_player.play("Idle")
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
